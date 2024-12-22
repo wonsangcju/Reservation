55b58787225f2b7323fb25948e2396a96e35a0cb
wonsangcju <wonsang200002@gmail.com>
2024-12-22 15:57:12 +0900

    예약 확인 패널 추가

--- src/Reservation/Main.java
+++ src/Reservation/Main.java
@@ -4,11 +4,15 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -29,19 +33,20 @@ import javax.swing.JPanel;
  *            </ul>
  */
 public class Main extends JFrame {
-	public JPanel mainPanel, reservationPanel;
-	
-	public JButton menuButton, seat1, seat2, seat3, seat4, seat5, seat6, seat7, seat8, seat9;
+	public JPanel mainPanel, reservationPanel, reservationCheckPanel;
+	public JLabel sentence;
+	public JButton menuButton, checkButton, seat1, seat2, seat3, seat4, seat5, seat6, seat7, seat8, seat9;
 	
 	public Main() {
 		this.setTitle("학생식당");
-		this.setSize(335, 400);
+		this.setSize(335, 460);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setBackground(Color.green);
 		
 		this.setLayout(new CardLayout());
 
-		//자리 예약을 할 수 있는 패널
+		mainPanel();
+		reservationCheckPanel();
 		
 		this.setVisible(true);
 	}
@@ -67,31 +72,71 @@ public class Main extends JFrame {
 		menuButton.setBorder(null);
 		topPanel.add(menuButton);
 		
+		
+		sentence = new JLabel("예약을 원하는 좌석을 선택해주세요.");
+		sentence.setFont(new Font("굴림 보통", Font.BOLD, 15));
+		sentence.setForeground(Color.white);
+		topPanel.add(sentence);
+		
 		reservationPanel.add(topPanel, BorderLayout.NORTH);
 		
+		//좌석 버튼들이 배치될 패널
 		JPanel seatPanel = new JPanel(new GridLayout(3, 3));
 		seatPanel.setBackground(Color.gray);
 		
+		seatButtonListener seatListener = new seatButtonListener();
+		checkButtonListener checkListener = new checkButtonListener();
+		
+		//좌석 버튼
 		seat1 = new JButton();
-		new decorateButton(seat1, seatPanel, "1번", 80, 80, 80);
+		new decorateButton(seat1, seatPanel, "1번", 80, 80, 80, seatListener);
 		seat2 = new JButton();
-		new decorateButton(seat2, seatPanel, "2번", 80, 80, 80);
+		new decorateButton(seat2, seatPanel, "2번", 80, 80, 80, seatListener);
 		seat3 = new JButton();
-		new decorateButton(seat3, seatPanel, "3번", 80, 80, 80);
+		new decorateButton(seat3, seatPanel, "3번", 80, 80, 80, seatListener);
 		seat4 = new JButton();
-		new decorateButton(seat4, seatPanel, "4번", 80, 80, 80);
+		new decorateButton(seat4, seatPanel, "4번", 80, 80, 80, seatListener);
 		seat5 = new JButton();
-		new decorateButton(seat5, seatPanel, "5번", 80, 80, 80);
+		new decorateButton(seat5, seatPanel, "5번", 80, 80, 80, seatListener);
 		seat6 = new JButton();
-		new decorateButton(seat6, seatPanel, "6번", 80, 80, 80);
+		new decorateButton(seat6, seatPanel, "6번", 80, 80, 80, seatListener);
 		seat7 = new JButton();
-		new decorateButton(seat7, seatPanel, "7번", 80, 80, 80);
+		new decorateButton(seat7, seatPanel, "7번", 80, 80, 80, seatListener);
 		seat8 = new JButton();
-		new decorateButton(seat8, seatPanel, "8번", 80, 80, 80);
+		new decorateButton(seat8, seatPanel, "8번", 80, 80, 80, seatListener);
 		seat9 = new JButton();
-		new decorateButton(seat9, seatPanel, "9번", 80, 80, 80);
+		new decorateButton(seat9, seatPanel, "9번", 80, 80, 80, seatListener);
 		
 		reservationPanel.add(seatPanel, BorderLayout.CENTER);
+		
+		JPanel checkPanel = new JPanel();
+		checkPanel.setBackground(Color.gray);
+		
+		checkButton = new JButton();
+		new decorateButton(checkButton, checkPanel, "예약", 80, 80, 80, checkListener);
+		checkButton.setPreferredSize(new Dimension(100, 50));
+		
+		reservationPanel.add(checkPanel, BorderLayout.SOUTH);
+	}
+	
+	public void reservationCheckPanel() {
+		reservationCheckPanel = new JPanel();
+		reservationCheckPanel.setBackground(Color.LIGHT_GRAY);
+		
+		this.add(reservationCheckPanel);
+	}
+	
+	public class seatButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			JButton clickSeat = (JButton) e.getSource();
+			sentence.setText(clickSeat.getText() + " 좌석이 선택되었습니다.");
+		}
+	}
+	
+	public class checkButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			
+		}
 	}
 	
 	public static void main(String[] args) {
