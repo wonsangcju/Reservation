3d9fc59590224d786c20f7deb5eb771a54571149
wonsangcju <wonsang200002@gmail.com>
2024-12-25 21:16:48 +0900

    로그인, 회원가입 리스너 추가

--- src/Reservation/Listeners.java
+++ src/Reservation/Listeners.java
@@ -1,378 +1,224 @@
 package Reservation;
 
-import java.awt.BorderLayout;
 import java.awt.CardLayout;
-import java.awt.Color;
 import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-/**
- * 학생식당 자리 예약 및 식단 확인 프로그램
- * 
- * @author 2021011865 전원상
- * @version 1.0
- * 
- * @created 2024.12.19
- * @lastModified 2024.12.22
- * 
- *
- * @changelog
- *            <ul>
- *            <li>2024.12.19 12:00 최초 생성</li>
- *            <li>2024.12.22 07:00 패널 별도의 메소드로 분리</li>
- *            <li>2024.12.22 08:00 메인패널을 메소드로 분리하고 다른 메소드들을 메인패널에 통합</li>
- *            <li>2024.12.23 23:00 리스너들을 별도의 클래스로 분리</li>
- *            </ul>
- */
-public class Main extends JFrame {
-	public CardLayout cardLayout;
-	public JPanel mainPanel, reservationPanel, reservationCheckPanel, loginPanel, registerPanel;
-	public JLabel sentence;
-	public JButton seat1, seat2, seat3, seat4, seat5, seat6, seat7, seat8, seat9;
-	public JButton menuButton, checkButton, homeButton, cancelButton;
-	public JTextField inputId;
-	public JPasswordField inputPassword;
-	public String selectedSeat = "";
+public class Listeners {
+	/**
+	 * 리스너를 모아놓은 클래스
+	 * 
+	 * @changelog
+	 *            <ul>
+	 *            <li>2024.12.23 23:00 최초 생성</li>
+	 *            </ul>
+	 */
+	private Main mainApp;
 
-	Listeners listeners;
-
-	public Main() {
-		listeners = new Listeners(this);
-
-		this.setTitle("학생식당");
-		this.setSize(400, 465);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setBackground(Color.green);
-
-		cardLayout = new CardLayout();
-		this.setLayout(cardLayout);
-		
-		mainPanel();
-		loginPanel();
-		registerPanel();
-		reservationCheckPanel();
-		
-		cardLayout.show(this.getContentPane(), "Login");
-
-		this.setVisible(true);
+	public Listeners(Main mainApp) {
+		this.mainApp = mainApp;
 	}
 
-	public void mainPanel() {
-		/**
-		 * 예약을 할 수 있고 메인이 되는 패널
-		 * 
-		 * @changelog
-		 *            <ul>
-		 *            <li>2024.12.24 07:00 별도의 메소드로 분리</li>
-		 *            <li>2024.12.22 08:00 다른 메소드들을 메인패널에 통합</li>
-		 *            <li>2024.12.24 15:00 로그인 패널 생성</li>
-		 *            <li>2024.12.24 22:00 회원가입 패널 생성</li>
-		 *            <ul>
-		 */
-		mainPanel = new JPanel();
-		mainPanel.setBackground(Color.black);
-
-		reservationPanel = new JPanel(new BorderLayout());
-		reservationPanel.setBackground(Color.gray);
-
-		mainPanel.add(reservationPanel);
-
-		this.add(mainPanel, "Main");
-
-		JPanel topPanel = new JPanel();
-		topPanel.setBackground(Color.gray);
-
-		// 메뉴 버튼
-		ImageIcon listIcon = new ImageIcon("img/menuButton.png");
-		menuButton = new JButton(listIcon);
-		menuButton.setPreferredSize(new Dimension(40, 40));
-		menuButton.setBorder(null);
-		topPanel.add(menuButton);
-
-		sentence = new JLabel("예약을 원하는 좌석을 선택해주세요.");
-		sentence.setFont(new Font("굴림 보통", Font.BOLD, 15));
-		sentence.setForeground(Color.white);
-		topPanel.add(sentence);
-
-		reservationPanel.add(topPanel, BorderLayout.NORTH);
-
-		// 좌석 버튼들이 배치될 패널
-		JPanel seatPanel = new JPanel(new GridLayout(3, 3));
-		seatPanel.setBackground(Color.gray);
-
-		// 좌석 버튼
-		seat1 = new JButton();
-		new decorateButton(seat1, seatPanel, "1번", 80, 80, 80, listeners.new SeatButtonListener());
-		seat2 = new JButton();
-		new decorateButton(seat2, seatPanel, "2번", 80, 80, 80, listeners.new SeatButtonListener());
-		seat3 = new JButton();
-		new decorateButton(seat3, seatPanel, "3번", 80, 80, 80, listeners.new SeatButtonListener());
-		seat4 = new JButton();
-		new decorateButton(seat4, seatPanel, "4번", 80, 80, 80, listeners.new SeatButtonListener());
-		seat5 = new JButton();
-		new decorateButton(seat5, seatPanel, "5번", 80, 80, 80, listeners.new SeatButtonListener());
-		seat6 = new JButton();
-		new decorateButton(seat6, seatPanel, "6번", 80, 80, 80, listeners.new SeatButtonListener());
-		seat7 = new JButton();
-		new decorateButton(seat7, seatPanel, "7번", 80, 80, 80, listeners.new SeatButtonListener());
-		seat8 = new JButton();
-		new decorateButton(seat8, seatPanel, "8번", 80, 80, 80, listeners.new SeatButtonListener());
-		seat9 = new JButton();
-		new decorateButton(seat9, seatPanel, "9번", 80, 80, 80, listeners.new SeatButtonListener());
-
-		reservationPanel.add(seatPanel, BorderLayout.CENTER);
-
-		// 예약확인, 취소 버튼이 있는 패널
-		JPanel checkPanel = new JPanel();
-		checkPanel.setBackground(Color.gray);
-
-		// 예약확인 버튼
-		checkButton = new JButton();
-		new decorateButton(checkButton, checkPanel, "예약", 80, 80, 80, listeners.new CheckButtonListener());
-		checkButton.setPreferredSize(new Dimension(100, 50));
-		reservationPanel.add(checkPanel, BorderLayout.SOUTH);
-
-		// 예약취소 버튼
-		cancelButton = new JButton();
-		new decorateButton(cancelButton, checkPanel, "예약 취소", 80, 80, 80, listeners.new CancelButtonListener());
-		cancelButton.setPreferredSize(new Dimension(100, 50));
-
+	// 좌석 버튼 리스너
+	public class SeatButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			JButton clickedSeat = (JButton) e.getSource();
+			mainApp.selectedSeat = clickedSeat.getText();
+			mainApp.sentence.setText(mainApp.selectedSeat + " 좌석이 선택되었습니다.");
+		}
 	}
 
-	public void reservationCheckPanel() {
-		/**
-		 * 예약 완료 패널
-		 * 
-		 * @changelog
-		 *            <ul>
-		 *            <li>2024.12.22 16:00 최초 생성</li>
-		 *            <li>2024.12.22 18:00 홈 버튼 추가</li>
-		 *            <ul>
-		 */
-		reservationCheckPanel = new JPanel();
-		reservationCheckPanel.setBackground(Color.LIGHT_GRAY);
-
-		// 예약 완료 문구
-		JLabel confirmLabel = new JLabel("예약이 완료되었습니다.");
-		confirmLabel.setFont(new Font("굴림 보통", Font.BOLD, 20));
-		confirmLabel.setHorizontalAlignment(JLabel.CENTER);
-		reservationCheckPanel.add(confirmLabel, BorderLayout.NORTH);
-
-		// 홈 버튼
-		JPanel homePanel = new JPanel();
-		homeButton = new JButton("");
-		new decorateButton(homeButton, homePanel, "홈으로", 80, 80, 80, listeners.new HomeButtonListener());
-		reservationCheckPanel.add(homePanel, BorderLayout.SOUTH);
-
-		this.add(reservationCheckPanel, "Confirm");
+	// 예약확인 버튼 리스너
+	public class CheckButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (!mainApp.selectedSeat.isEmpty()) {
+				mainApp.saveReservation(mainApp.selectedSeat);
+				mainApp.cardLayout.show(mainApp.getContentPane(), "Confirm");
+			} else {
+				mainApp.sentence.setText("좌석을 먼저 선택해주세요.");
+			}
+		}
 	}
 
-	public void loginPanel() {
-		/**
-		 * 로그인 패널
-		 * 
-		 * @changelog
-		 *            <ul>
-		 *            <li>2024.12.24 15:00 최초 생성</li>
-		 *            <ul>
-		 */
-		loginPanel = new JPanel();
-		loginPanel.setBackground(Color.white);
-		loginPanel.setLayout(new BorderLayout());
-
-		JPanel labelPanel = new JPanel(new GridLayout(2, 1));
-		JLabel idLabel = new JLabel("아이디");
-		labelPanel.add(idLabel);
-		JLabel pwLabel = new JLabel("비밀번호 ");
-		labelPanel.add(pwLabel);
-
-		JPanel inputPanel = new JPanel(new GridLayout(2, 1));
-		inputId = new JTextField(10);
-		inputPanel.add(inputId);
-		inputPassword = new JPasswordField(10);
-		inputPanel.add(inputPassword);
-		
-		JPanel loginRegisterPanel = new JPanel(new GridLayout(2, 1, 10, 10));
-		JButton loginButton = new JButton();
-		new decorateButton(loginButton, loginRegisterPanel, "로그인", 80, 80, 80,
-				listeners.new LoginButtonListener(inputId, inputPassword, loginPanel, cardLayout, mainPanel));
-		JButton registerButton = new JButton();
-		new decorateButton(registerButton, loginRegisterPanel, "회원가입", 80, 80, 80,
-				listeners.new RegisterButtonListener());
-
-		JPanel centerPanel = new JPanel(new BorderLayout());
-		centerPanel.add(labelPanel, BorderLayout.WEST);
-		centerPanel.add(inputPanel, BorderLayout.CENTER);
-		centerPanel.add(loginRegisterPanel, BorderLayout.EAST);
-		centerPanel.setBorder(javax.swing.BorderFactory.createEmptyBorder(100, 50, 100, 50));
-
-		loginPanel.add(centerPanel, BorderLayout.CENTER);
-
-		this.add(loginPanel, "Login");
+	// 예약취소 버튼 리스너
+	public class CancelButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (!mainApp.selectedSeat.isEmpty()) {
+				mainApp.cancelReservationInCSV(mainApp.selectedSeat);
+				mainApp.selectedSeat = "";
+				mainApp.sentence.setText("예약이 취소되었습니다.");
+				mainApp.cardLayout.show(mainApp.getContentPane(), "Main");
+			}
+		}
 	}
 
-	public void registerPanel() {
-		/**
-		 * 회원가입 패널
-		 * 
-		 * @changelog
-		 *            <ul>
-		 *            <li>2024.12.24 22:00 최초 생성</li>
-		 *            <ul>
-		 */
-		registerPanel = new JPanel(new BorderLayout());
-		registerPanel.setBackground(Color.white);
-
-		JLabel statusLabel = new JLabel("회원가입 정보를 입력해주세요.", JLabel.CENTER);
-		statusLabel.setFont(new Font("굴림", Font.BOLD, 16));
-		registerPanel.add(statusLabel, BorderLayout.NORTH);
-
-		JPanel centerPanel = new JPanel(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.insets = new Insets(10, 10, 10, 10);
-
-		JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 15, 10));
-
-		JLabel idLabel = new JLabel("아이디:");
-		JTextField txtId = new JTextField(20);
-
-		JLabel pwLabel = new JLabel("비밀번호:");
-		JPasswordField txtPassword = new JPasswordField(20);
-
-		// 아이디 중복 검사 버튼
-		JButton checkDuplicateButton = new JButton();
-		new decorateButton(checkDuplicateButton, centerPanel, "중복 검사", 80, 80, 80,
-				listeners.new CheckDuplicateListener(txtId, registerPanel));
-		checkDuplicateButton.setFont(new Font("Seoge UI", Font.PLAIN, 13));
-		
-		// 회원가입 완료 버튼
-		JButton confirmButton = new JButton();
-		new decorateButton(confirmButton, buttonPanel, "<html><div style='text-align: center;'>회원가입<br>완료</div></html>",
-				80, 80, 80, listeners.new ConfirmRegisterListener(txtId, txtPassword, registerPanel, cardLayout,
-						this.getContentPane()));
-		
-		// 회원가입 취소 버튼
-		JButton cancelButton = new JButton();
-		new decorateButton(cancelButton, buttonPanel, "취소", 80, 80, 80,
-				e -> cardLayout.show(this.getContentPane(), "Login"));
-
-		JLabel gapLabel = new JLabel("");
-
-		Dimension textFieldSize = new Dimension(50, 20);
-		txtId.setPreferredSize(textFieldSize);
-		txtPassword.setPreferredSize(textFieldSize);
-
-		// 회원가입 패널 배치
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.weightx = 0;
-		gbc.anchor = GridBagConstraints.EAST;
-		centerPanel.add(idLabel, gbc);
-
-		gbc.gridx = 1;
-		gbc.weightx = 1.0;
-		gbc.anchor = GridBagConstraints.WEST;
-		centerPanel.add(txtId, gbc);
-
-		gbc.gridx = 2;
-		gbc.weightx = 0;
-		gbc.anchor = GridBagConstraints.EAST;
-		centerPanel.add(checkDuplicateButton, gbc);
-
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		gbc.anchor = GridBagConstraints.WEST;
-		centerPanel.add(pwLabel, gbc);
-
-		gbc.gridx = 1;
-		centerPanel.add(txtPassword, gbc);
-
-		gbc.gridx = 2;
-		centerPanel.add(gapLabel, gbc);
-
-		registerPanel.add(centerPanel, BorderLayout.CENTER);
-		registerPanel.add(buttonPanel, BorderLayout.SOUTH);
-
-		this.add(registerPanel, "Register");
+	// 홈 버튼 리스너
+	public class HomeButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			mainApp.cardLayout.show(mainApp.getContentPane(), "Main");
+		}
 	}
 
-	public void saveReservation(String seat) {
-		/**
-		 * 예약 시 예약시간과 예약한 좌석이 csv에 작성되게 하는 메소드
-		 * 
-		 * @changelog
-		 *            <ul>
-		 *            <li>2024.12.23 22:00 최초 생성</li>
-		 *            <ul>
-		 */
-		String filePath = "seat/reservations.csv";
-		LocalDateTime now = LocalDateTime.now();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-		String timestamp = now.format(formatter);
+	// 로그인 버튼 리스너
+	public class LoginButtonListener implements ActionListener {
+	    private JTextField inputId;
+	    private JPasswordField inputPassword;
+	    private JPanel loginPanel, mainPanel;
+	    private CardLayout cardLayout;
 
-		String data = timestamp + "," + seat + "\n";
+	    public LoginButtonListener(JTextField inputId, JPasswordField inputPassword, JPanel loginPanel,
+	                               CardLayout cardLayout, JPanel mainPanel) {
+	        this.inputId = inputId;
+	        this.inputPassword = inputPassword;
+	        this.loginPanel = loginPanel;
+	        this.cardLayout = cardLayout;
+	        this.mainPanel = mainPanel;
+	    }
 
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+	        String id = inputId.getText().trim();
+	        String password = new String(inputPassword.getPassword()).trim();
+
+	        if (id.isEmpty() || password.isEmpty()) {
+	            JOptionPane.showMessageDialog(loginPanel, "아이디와 비밀번호를 모두 입력해주세요.", "경고", JOptionPane.WARNING_MESSAGE);
+	            return;
+	        }
+
+	        if (isLoginValid(id, password)) {
+	            JOptionPane.showMessageDialog(loginPanel, "로그인에 성공했습니다.", "성공", JOptionPane.INFORMATION_MESSAGE);
+	            cardLayout.show(mainPanel, "Main");
+	        } else {
+	            JOptionPane.showMessageDialog(loginPanel, "아이디 또는 비밀번호가 일치하지 않습니다.", "오류", JOptionPane.ERROR_MESSAGE);
+	        }
+	    }
+	}
+	
+	// 아이디, 비밀번호 확인 메소드
+	private boolean isLoginValid(String id, String password) {
+	    String filePath = "account/account.csv";
+	    try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+	        String line;
+	        while ((line = reader.readLine()) != null) {
+	            String[] userData = line.split(",");
+	            if (userData.length == 2 && userData[0].equals(id) && userData[1].equals(password)) {
+	                return true;
+	            }
+	        }
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    return false;
+	}
+
+	// 회원가입 버튼 리스너
+	public class RegisterButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			mainApp.cardLayout.show(mainApp.getContentPane(), "Register");
+		}
+	}
+
+	// 중복 확인 리스너
+	public class CheckDuplicateListener implements ActionListener {
+		private JTextField idField;
+		private JPanel registerPanel;
+
+		public CheckDuplicateListener(JTextField idField, JPanel registerPanel) {
+			this.idField = idField;
+			this.registerPanel = registerPanel;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String id = idField.getText().trim();
+			if (id.isEmpty()) {
+				JOptionPane.showMessageDialog(registerPanel, "아이디를 입력해주세요.", "경고", JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+
+			if (isDuplicateId(id)) {
+				JOptionPane.showMessageDialog(registerPanel, "이미 존재하는 아이디입니다.", "중복 확인", JOptionPane.ERROR_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(registerPanel, "사용 가능한 아이디입니다.", "중복 확인",
+						JOptionPane.INFORMATION_MESSAGE);
+			}
+		}
+	}
+
+	// 회원가입 리스너
+	public class ConfirmRegisterListener implements ActionListener {
+		private JTextField idField;
+		private JPasswordField passwordField;
+		private JPanel registerPanel;
+		private CardLayout cardLayout;
+		private Container mainContentPanel;
+
+		public ConfirmRegisterListener(JTextField idField, JPasswordField passwordField, JPanel registerPanel,
+				CardLayout cardLayout, Container mainContentPanel) {
+			this.idField = idField;
+			this.passwordField = passwordField;
+			this.registerPanel = registerPanel;
+			this.cardLayout = cardLayout;
+			this.mainContentPanel = mainContentPanel;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String id = idField.getText().trim();
+			String password = new String(passwordField.getPassword()).trim();
+
+			if (id.isEmpty() || password.isEmpty()) {
+				JOptionPane.showMessageDialog(registerPanel, "아이디와 비밀번호를 모두 입력해주세요.", "경고",
+						JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+
+			if (isDuplicateId(id)) {
+				JOptionPane.showMessageDialog(registerPanel, "아이디가 중복되었습니다.", "오류", JOptionPane.ERROR_MESSAGE);
+			} else {
+				saveUser(id, password);
+				JOptionPane.showMessageDialog(registerPanel, "회원가입이 완료되었습니다.", "완료", JOptionPane.INFORMATION_MESSAGE);
+				cardLayout.show(mainContentPanel, "Login");
+			}
+		}
+	}
+
+	// 회원가입 시 아이디 중복 여부 확인 메소드
+	private boolean isDuplicateId(String id) {
+		String filePath = "account/account.csv";
+		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				if (line.split(",")[0].equals(id)) {
+					return true;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	// 사용자 정보 저장 메소드
+	private void saveUser(String id, String password) {
+		String filePath = "account/account.csv";
+		String data = id + "," + password + "\n";
 		try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
 			writer.write(data);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
-	public void cancelReservationInCSV(String seat) {
-		/**
-		 * 예약을 취소할 경우 예약 시 작성됐던 내용이 csv에서 삭제되는 메소드
-		 * 
-		 * @changelog
-		 *            <ul>
-		 *            <li>2024.12.23 22:00 최초 생성</li>
-		 *            <ul>
-		 */
-		String filePath = "seat/reservations.csv";
-		StringBuilder updatedData = new StringBuilder();
-
-		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-			String line;
-			while ((line = reader.readLine()) != null) {
-				if (!line.contains("," + seat)) { // 선택된 좌석이 아닌 경우만 추가
-					updatedData.append(line).append("\n");
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, false))) {
-			writer.write(updatedData.toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args) {
-		new Main();
-	}
-
 }
